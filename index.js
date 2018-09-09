4ed34d13f1aa05d9022e99d91933d5cae78dd39e
Jantastication O <georginaoregx@gmail.com>
2018-09-09 22:20:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+happyHolidays('Happy Holidays!')
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+happyHolidaysTo("Happy holidays, you!")
